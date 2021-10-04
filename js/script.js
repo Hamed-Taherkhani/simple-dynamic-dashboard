2571c19b3615a931b987af0f1ec7d951605b9b62
Hamed Taherkhani <hamedtahi7@gmail.com>
2021-10-04 11:40:12 +0330

    Style scroll bar nad some script

--- js/script.js
+++ js/script.js
@@ -1,23 +1,47 @@
 const main = document.querySelector("main"),
   navItems = document.querySelectorAll("aside nav ul li");
 
+// To load dashboard as default page:
+document.body.onload = () => loadDoc("dashboard", "html", placeHtml);
+
 navItems.forEach((value) => {
-  value.addEventListener("click", loadDoc);
+  value.addEventListener("click", configRequest);
 });
 
-function loadDoc() {
-  const xhr = new XMLHttpRequest(),
-    fileName = this.children[1].textContent.toLowerCase();
-  url = "./Single page/" + fileName + ".txt";
-  console.log(url);
-  xhr.onload = () => placeOnPage(xhr.responseText);
-  xhr.open("GET", url, true);
+let fileName;
+function configRequest() {
+  fileName = this.children[1].textContent.toLowerCase();
+  loadDoc(fileName, "html", placeHtml);
+}
+
+function loadDoc(fileName, type, callBackFunction) {
+  let URL = "./Pages/";
+  if (type === "html") URL += fileName + "." + type;
+  if (type === "css" || type === "js")
+    URL += type + "/" + fileName + "." + type;
+
+  const xhr = new XMLHttpRequest();
+  xhr.onload = () => callBackFunction(xhr.responseText, fileName);
+  xhr.open("GET", URL, true);
   xhr.send();
 }
 
-function placeOnPage(res) {
-  console.log(res);
-  main.innerHTML = res;
-  const script = document.getElementById("script");
-  eval(script.textContent);
+function placeHtml(response, fileName) {
+  main.innerHTML = response;
+  loadDoc(fileName, "css", placeCss);
+}
+
+function placeCss(response, fileName) {
+  const styleTag = document.getElementById("style__");
+  styleTag.innerHTML = response;
+  loadDoc(fileName, "js", placeJS);
+}
+
+function placeJS(response) {
+  // Run the response string as a executable javascript code.
+  const cdns = document.getElementsByClassName("js-cdn");
+  let script = document.createElement("script");
+  script.setAttribute("src", cdns[0].src);
+  main.appendChild(script);
+  script.onload = () => eval(response);
 }
